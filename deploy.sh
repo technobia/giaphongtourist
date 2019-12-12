48afac054fb4c04cb99a18859198ce86bae0f33b
Binh Tran <binh.tran@nfq.asia>
2019-12-13 00:12:42 +0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
 # build
+echo "> remove old build version"
+rm -rf build/ docs/
+echo "> running build"
 yarn build
-cp CNAME docs/
+echo "> copy CNAME file"
+cp CNAME build/
+echo "> rename build folder to docs"
+mv build/ docs/
 
 # deploy
+echo "> deploying..."
 git add . &&
 git commit -m 'deploy' &&
 git push
