79e71204d0e9e944a81eeecef9869b3366973573
Binh Tran <technobia@gmail.com>
2020-05-26 11:38:14 +0700

    Updates

---
+++ precache-manifest.3265bfedafbee7f8188ca12fc87ce683.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d23d9f10729be9367d15bd72f4aa81",
+    "url": "giaphongtourist.com/index.html"
+  },
+  {
+    "revision": "f3251a8df891ef2add76",
+    "url": "giaphongtourist.com/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "875109c783ffba08adaf",
+    "url": "giaphongtourist.com/static/js/2.6f71ed66.chunk.js"
+  },
+  {
+    "revision": "650d25840fbc522695388d525eaf2727",
+    "url": "giaphongtourist.com/static/js/2.6f71ed66.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b038985e13bb61da434b",
+    "url": "giaphongtourist.com/static/js/3.29291210.chunk.js"
+  },
+  {
+    "revision": "f73e810844fba009bd106bf78cf93a52",
+    "url": "giaphongtourist.com/static/js/3.29291210.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55c9337281c5f349debd",
+    "url": "giaphongtourist.com/static/js/4.411d292c.chunk.js"
+  },
+  {
+    "revision": "1806efd52bc7eb3f6e9d",
+    "url": "giaphongtourist.com/static/js/5.8bb1892e.chunk.js"
+  },
+  {
+    "revision": "f7546f8576b22eb901c3",
+    "url": "giaphongtourist.com/static/js/6.ebc9b9a1.chunk.js"
+  },
+  {
+    "revision": "ee1c2f597a1b11603c0e",
+    "url": "giaphongtourist.com/static/js/7.8e94e2f2.chunk.js"
+  },
+  {
+    "revision": "994e776ba1804ff019be",
+    "url": "giaphongtourist.com/static/js/8.ec5a48c5.chunk.js"
+  },
+  {
+    "revision": "f3251a8df891ef2add76",
+    "url": "giaphongtourist.com/static/js/main.40979a9a.chunk.js"
+  },
+  {
+    "revision": "4fb5b30a83abeb8b205a",
+    "url": "giaphongtourist.com/static/js/runtime-main.87ba010f.js"
+  }
+]);
